5de260ed9a3840191be92a137e7f765e3e016973
nibardoreyes <81846099+nibardoreyes@users.noreply.github.com>
2023-12-03 21:43:47 -0700

    Update clock.c

--- clock.c
+++ clock.c
@@ -24,6 +24,7 @@ Description	:	Alarm Clock Application
 #include "bcd2seven.h"
 #include "clock.h"
 
+volatile signed int *HEX_ptr2; // Global definition of HEX_ptr2
 //===Defines====
 #define HW_REGS_BASE ( ALT_STM_OFST )
 #define HW_REGS_SPAN ( 0x04000000 )
@@ -34,14 +35,29 @@ void * map_physical (int, unsigned int, unsigned int);
 void close_physical (int);
 int unmap_physical (void *, unsigned int);
 //==================
-
+//the following union is for the switches
+typedef union{
+	unsigned int value;
+	struct{
+		unsigned int sw0:1;	//lsb-switch on the left
+		unsigned int sw1:1;
+		unsigned int sw2:1;
+		unsigned int sw3:1;
+		unsigned int sw4:1;
+		unsigned int sw5:1;
+		unsigned int sw6:1;
+		unsigned int sw7:1;
+		unsigned int sw8:1;
+		unsigned int sw9:1;
+	}bits;
+}Switches;
 
 int main(void)
 {
    volatile signed int * LEDR_ptr;   // virtual address pointer to red LEDs
    volatile signed int * HEX_ptr;	//virutal address pointer to Hex Dispay
-   volatile signed int * HEX_ptr2;
    volatile signed int * SW_ptr;	///address pointer to the switches
+   volatile signed int *KEY_ptr;     // virtual address for the KEY port
    int fd = -1;               // used to open /dev/mem for access to physical addresses
    void *LW_virtual;          // used to map physical addresses for the light-weight bridge
    LCD_CANVAS LcdCanvas;
@@ -60,16 +76,25 @@ int main(void)
 
    //====Stuff for 7-Segment
    // Set virtual address pointer to I/O port
-      LEDR_ptr = (unsigned int *) (LW_virtual + LEDR_BASE);
-      HEX_ptr = (unsigned int *)(LW_virtual + HEX3_HEX0_BASE);
-      HEX_ptr2 = (unsigned int *)(LW_virtual + HEX5_HEX4_BASE);
-      SW_ptr = LW_virtual + SW_BASE;//switch
+      LEDR_ptr = (signed int *) (LW_virtual + LEDR_BASE);
+      HEX_ptr = (signed int *)(LW_virtual + HEX3_HEX0_BASE);
+      HEX_ptr2 = (signed int *)(LW_virtual + HEX5_HEX4_BASE);
+      SW_ptr = (signed int *)(LW_virtual + SW_BASE);//switch
+      KEY_ptr =(signed int *)( LW_virtual + KEY_BASE);    // in it virtual address for KEY port
 
+
+      //Registers=============
       DataRegister dataRegister;
       dataRegister.value = 0;
-
       DataRegister2 dataRegister2;
       dataRegister2.value = 0;
+
+      PushButton button;
+      button.value = 0;
+      Switches switch1;
+      switch1.value = 0;
+
+
 ///==============================
 
 //======LCD Stuff========
@@ -77,8 +102,6 @@ int main(void)
   	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
 
 //========================
-
-
 
    //===========================================
    //LCD Intro Message
@@ -123,21 +146,69 @@ int main(void)
    	//============================================================
    	*LEDR_ptr = 0;
    	*HEX_ptr = 0;
-
+   	int counter5 = 0;
+   	int counter4 = 0;
+   	int counter3 = 0;
+   	int counter2 = 0;
    	while(1){
-   	   if(*SW_ptr == 0){
+   	//read first switch orientation
+   	switch1.value = *SW_ptr;
+   	   if(switch1.bits.sw0 == 0){
    		dataRegister.bits.hex1 = bcd2sevenSegmentDecoder(0);
    		dataRegister.bits.hex0 = bcd2sevenSegmentDecoder(0);
    		dataRegister.bits.hex3 = bcd2sevenSegmentDecoder(0);
    		dataRegister.bits.hex2 = bcd2sevenSegmentDecoder(0);
    		dataRegister2.bits.hex5 = bcd2sevenSegmentDecoder(0);
    		dataRegister2.bits.hex4 = bcd2sevenSegmentDecoder(0);
-
    		*HEX_ptr = dataRegister.value;
    		*HEX_ptr2 = dataRegister2.value;
+   		//====Setting up clock=====
+   		//display 5
+   		button.value = *KEY_ptr;
+   		        if (button.bits.key3 == 1) {
+   		            // Increment the counter if the button is pressed
+   		            counter5++;
+   		        }
+   		         dataRegister2.bits.hex5 = bcd2sevenSegmentDecoder(counter5);
+   		      *HEX_ptr2 = dataRegister2.value;
+   		            if (counter5 == 3) {
+   		                counter5 = 0; // Reset the counter to 0 when it reaches 10
+   		            }
+   		            //display 4
+			 if (button.bits.key2 == 1) {
+			// Increment the counter if the button is pressed
+			counter4++;
+			}
+			dataRegister2.bits.hex4 = bcd2sevenSegmentDecoder(counter4);
+			*HEX_ptr2 = dataRegister2.value;
+			if (counter4 == 10) {
+			counter4 = 0; // Reset the counter to 0 when it reaches 10
+			}
+			//display 3
+						 if (button.bits.key1 == 1) {
+						// Increment the counter if the button is pressed
+						counter3++;
+						}
+						dataRegister.bits.hex3 = bcd2sevenSegmentDecoder(counter3);
+						*HEX_ptr = dataRegister.value;
+						if (counter3 == 10) {
+						counter3 = 0; // Reset the counter to 0 when it reaches 10
+						}
+						//display 2
+									 if (button.bits.key0 == 1) {
+									// Increment the counter if the button is pressed
+									counter2++;
+									}
+									dataRegister.bits.hex2 = bcd2sevenSegmentDecoder(counter2);
+									*HEX_ptr = dataRegister.value;
+									if (counter2 == 10) {
+									counter2 = 0; // Reset the counter to 0 when it reaches 10
+									}
 
-   	   }else{
+   	   }
 
+
+   	   if (switch1.bits.sw0 == 1){
    		int hour1 = getHour() /10;//first digit
    		int hour2 = getHour() % 10;//second digit
    		int min1 = getMinute() / 10;
@@ -154,6 +225,8 @@ int main(void)
    		*HEX_ptr = dataRegister.value;
    		*HEX_ptr2 = dataRegister2.value;
    	   }
+
+
    	   sleep(1);
    	   }//end of while loop
    //============================================================
@@ -176,9 +249,6 @@ int main(void)
 //============================================================
 //===Alarm Stuff Below======
 //============================================================
-
-
-
 
 
 
@@ -242,3 +312,5 @@ int unmap_physical(void * virtual_base, unsigned int span)
    }
    return 0;
 }
+
+
