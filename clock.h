d76e69ca9863dfb477fa48af10b3f11b6a9bbaca
nibardoreyes <81846099+nibardoreyes@users.noreply.github.com>
2023-12-05 08:25:19 -0700

    Add files via upload

--- clock.h
+++ clock.h
@@ -12,7 +12,12 @@
 #include <time.h>
 #include <sys/mman.h>
 
+time_t programStartTime;
+int initialHour = 0;
+int initialMinute = 0;
+int initialSecond = 0;
 
+volatile signed int * LEDR_ptr;   // virtual address pointer to red LEDs
 //First 2 unions are for the 7 segment displays
 typedef union
 {
@@ -53,6 +58,22 @@ typedef union{
 	}bits;
 }PushButton;
 
+//the following union is for the switches
+typedef union{
+	unsigned int value;
+	struct{
+		unsigned int sw0:1;	//lsb-switch on the left
+		unsigned int sw1:1;
+		unsigned int sw2:1;
+		unsigned int sw3:1;
+		unsigned int sw4:1;
+		unsigned int sw5:1;
+		unsigned int sw6:1;
+		unsigned int sw7:1;
+		unsigned int sw8:1;
+		unsigned int sw9:1;
+	}bits;
+}Switches;
 
 
 int getHour() {
@@ -80,10 +101,84 @@ int getSecond() {
     return timeinfo->tm_sec;
 }
 
+int setHour(int newHour) {
+    time_t now;
+    struct tm *timeinfo;
+
+    time(&now);
+    timeinfo = localtime(&now);
+    timeinfo->tm_hour = newHour;
+    mktime(timeinfo); // Update the time using mktime function
+    return timeinfo->tm_hour;
+}
+
+int setMinute(int newMinute) {
+    time_t now;
+    struct tm *timeinfo;
+
+    time(&now);
+    timeinfo = localtime(&now);
+    timeinfo->tm_min = newMinute;
+    mktime(timeinfo); // Update the time using mktime function
+    return timeinfo->tm_min;
+}
+
+int setSecond(int newSecond) {
+    time_t now;
+    struct tm *timeinfo;
+
+    time(&now);
+    timeinfo = localtime(&now);
+    timeinfo->tm_min = newSecond;
+    mktime(timeinfo); // Update the time using mktime function
+    return timeinfo->tm_sec;
+}
+
+int combineNumbers(int num1, int num2) {
+    int combined;
+
+    combined = num1 * 10 + num2;
+
+    return combined;
+}
+
+void updateTime(int newHour, int newMinute, int newSecond) {
+    setHour(newHour);
+    setMinute(newMinute);
+    setSecond(newSecond);
+}
+
+void fetchUpdatedTime(int *newHour, int *newMinute, int *newSecond) {
+    *newHour = getHour();
+    *newMinute = getMinute();
+    *newSecond = getSecond();
+}
 
 
+void startClock(int hour, int minute, int second) {
+    programStartTime = time(NULL); // Record the program start time
+    initialHour = hour;
+    initialMinute = minute;
+    initialSecond = second;
+}
 
+int getHour2() {
+    time_t now = time(NULL);
+    double secondsElapsed = difftime(now, programStartTime);
+    return ((int)secondsElapsed / 3600 + initialHour) % 24; // Calculate hours
+}
 
+int getMinute2(){
+    time_t now = time(NULL);
+    double secondsElapsed = difftime(now, programStartTime);
+    return ((int)secondsElapsed / 60 + initialMinute) % 60; // Calculate minutes
+}
+
+int getSecond2() {
+    time_t now = time(NULL);
+    double secondsElapsed = difftime(now, programStartTime);
+    return ((int)secondsElapsed + initialSecond) % 60; // Calculate seconds
+}
 
 
 
